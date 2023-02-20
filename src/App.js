e436d20da0118bd47b15931108c66f10ec4fee1b
AKILESH <100372708+AKILESHMONISH@users.noreply.github.com>
2023-02-20 13:40:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+//import logo from './logo.svg';
+import './App.css';
+import MyButton from './component/Button';
+function App() {
+  return (
+    <div>
+      <h1>Samsung</h1>
+      <h3>Samsung Galaxy S23 Ultra- Full Specification, Price</h3>
+      <img src="https://images.samsung.com/in/smartphones/galaxy-s23-ultra/buy/DM3-web-1.jpg"></img>
+      <img src="https://images.samsung.com/is/image/samsung/assets/in/smartphones/galaxy-s23/buy/1440x450_1.jpg"></img>
+      <br></br>
+      <MyButton/>
+      <p>Samsung launched the Galaxy S10 series on 20 February 2019 at its Unpacked event in San Francisco. The company’s new flagship devices includes the Galaxy S10e, S10 and high-end S10 plus. with all-new in-display ultrasonic fingerprint sensor, amazing display, reverse wireless charging and great design, Samsung galaxy S10 edge will be the best smartphone in flagship segment.
+you are find much more upgrades on this Samsung device that didn’t have in its predecessor Galaxy S9. there a lot to talk about this feature rich phone, So here you will get to know everything about Galaxy S10 edge.</p>
+   <h5 class="A">Display: 6.8 inches (17.27 cm)<br></br>, Edge Quad HD+,<br></br> 120 Hz Refresh Rate
+<br></br>Memory: 12GB RAM, 256GB ROM<br></br>
+Processor: Qualcomm Snapdragon 8 Gen 2, Octa-Core<br></br>
+Camera: 200MP + 10MP + 12MP + 10MP Quad Rear & 12MP Front Camera
+<br></br>
+Battery: 5000mAh
+USP: Fingerprint Sensor</h5>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Button.js
@@ -0,0 +1,9 @@
+import "./Button.css";
+
+function MyButton(){
+    return(
+        <button>BUY NOW THIS ONLY FOR 1,34,000</button>
+    );
+}
+
+export default MyButton;
